43a8d3359048d56a1fe1fdd8a65bfa151a268289
omerUnver <muhammedomerunver@gmail.com>
2022-04-14 13:27:11 +0300

    positiveOrNegativeNumber

--- positiveOrNegativeNumber/ViewController.swift
+++ positiveOrNegativeNumber/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var sayiText: UITextField!
+    @IBOutlet weak var sonucText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //Girilen sayının negatif, pozitif veya sıfır olduğunu gösteren bir çalışma
     }
-
+    @IBAction func hesaplaBtn(_ sender: Any) {
+        let number = sayiText.text ?? "" //default boş string atadık
+        if number.isEmpty {
+            sonucText.text = "bir sayı giriniz"
+            return
+        }
+        if let n1 = Int(number) {
+            if n1 > 0 {
+                sonucText.text = "girilen sayı pozitif"
+            }else if n1 < 0 {
+                sonucText.text = "girilen sayı bir negatiftir"
+            }else{
+                sonucText.text = "girilen sayı sıfırdır"
+            }
+                    
+        }else {
+            sonucText.text = "bir rakam giriniz"
+        }
+    }
 
 }
 
